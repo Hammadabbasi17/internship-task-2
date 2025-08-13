753cf306e4444ec2ca2727f0adc33e1fd55b3d69
hammadabbasi17 <hammadabbasi1700@gmail.com>
2025-08-13 22:02:32 +0500

    first commit

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 const menu = document.getElementById('toggle-btn');
 const navlinks = document.getElementById('navlinks');
-menu.addEventListener('click',()=>{
-    navlinks.classList.toggle('active')
-    if(navlinks.className == 'active'){
-      document.getElementById("bar").className = "ri-close-line";
-    } else {
-      document.getElementById("bar").className = "ri-menu-line";
+menu.addEventListener('click', () => {
+  navlinks.classList.toggle('active')
+  if (navlinks.className == 'active') {
+    document.getElementById("bar").className = "ri-close-line";
+  } else {
+    document.getElementById("bar").className = "ri-menu-line";
+
+  }
+
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".progress-bar").forEach(bar => {
+    let targetWidth = bar.getAttribute("data-width");
+    setTimeout(() => {
+      bar.style.width = targetWidth + "%";
+    }, 200); // Delay for smoother start
+  });
+
+});
+function submit() {
+
+
+
+  let form = document.getElementById('emailform');
+  form.addEventListener('submit', (e) => {
+    if (!form.value.includes('@')) {
+      e.preventDefault();
+      alert('Please enter a valid email');
 
     }
 
-});
+  })
+}
